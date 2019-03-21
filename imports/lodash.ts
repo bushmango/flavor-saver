3fcaf23250e05fd17e752d44fb0dec97ce659ac0
sbushman <steve@stevebushman.com>
2019-03-21 08:58:49 -0500

    first edition

---
+++ imports/lodash.ts
@@ -0,0 +1,5 @@
+import assign from 'lodash/assign'
+import pick from 'lodash/pick'
+
+const _ = { pick, assign }
+export { _ }
---
+++ index.ts
@@ -0,0 +1,60 @@
+import { _ } from './imports/lodash'
+
+function log(...x) {
+  if (console && console.log) {
+    console.log('flavor-saver', ...x)
+  }
+}
+
+export interface IFlavorSaver<T> {
+  localStorageKey: string
+  // fields: [keyof T]
+  save: (t: T) => void
+  restore: (t: T) => T
+}
+
+export function create<T>(
+  key: string,
+  version: string,
+  fields?: [keyof T]
+): IFlavorSaver<T> {
+  const localStorageKey = key + ':' + version
+  return {
+    localStorageKey: localStorageKey,
+    save: (t: T) => {
+      save(t, localStorageKey, fields)
+    },
+    restore: (t: T) => {
+      return restore(t, localStorageKey, fields)
+    },
+  }
+}
+
+function save<T>(t: T, localStorageKey, fields?: [keyof T]) {
+  if (typeof localStorage !== 'undefined') {
+    let picked = t
+    if (fields) {
+      picked = _.pick(t, fields)
+    }
+    localStorage.setItem(localStorageKey, JSON.stringify(picked))
+  }
+}
+
+function restore<T>(t: T, localStorageKey, fields?: [keyof T]) {
+  if (typeof localStorage !== 'undefined') {
+    try {
+      let stored = localStorage.getItem(localStorageKey)
+      if (stored) {
+        // Get only picked fields
+        let parsed = JSON.parse(stored)
+        let picked = parsed
+        if (fields) {
+          picked = _.pick(parsed, fields)
+        }
+        return _.assign({}, t, picked)
+      }
+    } catch (err) {
+      log('Error loading state from localStorage: ' + localStorageKey)
+    }
+  }
+}
